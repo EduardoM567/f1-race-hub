f69fc71fe3e8d0f3402c535acf55bbc8a4cf7f73
EduardoM567 <em567@njit.edu>
2026-06-28 11:44:19 -0400

    em567: Add config file for RabbitMQ settings

---
+++ api/config.py
@@ -0,0 +1,9 @@
+# RabbitMQ Configuration
+# Owner: Eduardo (em567)
+RABBITMQ_HOST = '100.122.133.62'
+RABBITMQ_PORT = 5672
+RABBITMQ_USER = 'teamuser'
+RABBITMQ_PASS = 'password123'
+LOG_EXCHANGE = 'log_exchange'
+LOG_ROUTING_KEY = 'log'
+LOG_FILE = '/home/em567/api_vm.log'
