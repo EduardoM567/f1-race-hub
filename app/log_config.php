5255bab8fd58f8fab196ec8c2512f9c336f971e2
ruchirexe <gonxkillua44@gmail.com>
2026-06-29 02:17:16 +0000

    Add APP VM logging

---
+++ app/log_config.php
@@ -0,0 +1,22 @@
+<?php
+#lets PHP know about the RabbitMQ library
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+#passes on env values
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('MQ_HOST', $_ENV['MQ_HOST']);
+define('MQ_PORT', (int) $_ENV['MQ_PORT']);
+define('MQ_USER', $_ENV['MQ_USER']);
+define('MQ_PASS', $_ENV['MQ_PASS']);
+define('MQ_VHOST', $_ENV['MQ_VHOST']);
+define('LOG_EXCHANGE', $_ENV['LOG_EXCHANGE']);
+define('LOG_ROUTING_KEY', $_ENV['LOG_ROUTING_KEY']);
+define('DLQ_QUEUE', $_ENV['DLQ_QUEUE']);
+
+#opens rabbitmq connection
+function get_mq_connection(){
+    return new AMQPStreamConnection(MQ_HOST, MQ_PORT, MQ_USER, MQ_PASS, MQ_VHOST);
+}
---
+++ app/publish_log.php
@@ -0,0 +1,54 @@
+<?php
+require_once __DIR__ . '/log_config.php';
+require_once __DIR__ . '/route_dlq.php';
+
+use PhpAmqpLib\Message\AMQPMessage;
+
+#main logging function takes log level and message
+function publish_log(string $type, string $message):void{
+    $allowed = ['info', 'warning', 'error'];
+
+    #check if message is valid before sending if not sends to DLQ
+    if(empty($type) || empty($message) || !in_array($type, $allowed)){
+        error_log("[APP-LOGGER] bad log event, sending to DLQ");
+        route_to_dlq(['type' => $type, 'message' => $message]);
+        return;
+    }
+
+    #makes the json message
+    $payload = json_encode([
+        'source' => gethostname(),
+        'timestamp' => date('Y-m-d\TH:i:s'),
+        'level' => strtoupper($type),
+        'message' => $message
+    ]);
+
+    try{
+        #reopens channel and connection again
+        $conn=get_mq_connection();
+        $ch=$conn->channel();
+
+        # makes the exchange  
+        $ch->exchange_declare(LOG_EXCHANGE, 'direct', false, true, false);
+
+        #wraps playload in a message
+        $msg=new AMQPMessage($payload,[
+            'content_type' => 'application/json',
+            'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT
+        ]);
+
+        #sends to the echange with the routing key
+        $ch->basic_publish($msg, LOG_EXCHANGE, LOG_ROUTING_KEY);
+        echo "[APP-LOGGER] sent: $payload\n";
+
+        #closes channel and connection
+        $ch->close();
+        $conn->close();
+
+    
+    }catch(Exception $e){
+        # if any error happesn during the logging process goes into here and to DLQ
+        error_log("[APP-LOGGER] failed to send: " . $e->getMessage());
+        route_to_dlq(['type' => $type, 'message' => $message]);
+    }
+}
